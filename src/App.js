734ce7b61ac7480e4bbf6ca6b802c20bc7689ecf
Mariusz Wojtkiewicz <wojtkiewicz89@gmail.com>
2021-03-29 16:28:11 +0200

    add styles to header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navigation from "./assets/components/Navigation/Navigation";
+import Header from "./assets/components/Header/Header";
 
 function App() {
   return (
     <>
       <Navigation />
+      <Header />
     </>
   );
 }
--- src/assets/components/Header/Header.js
+++ src/assets/components/Header/Header.js
@@ -6,7 +6,9 @@ const Header = () => (
     <div className={styles.header__wrapper}>
       <div className={styles.progress__container}></div>
       <div className={styles.week__container}></div>
-      <div classname={styles.selector__container}></div>
+      <div className={styles.selector__container}></div>
     </div>
   </>
 );
+
+export default Header;
