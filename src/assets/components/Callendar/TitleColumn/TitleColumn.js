7829e3d15749537594e8c70a89182f8f154cc59c
Mariusz Wojtkiewicz <wojtkiewicz89@gmail.com>
2021-03-30 18:10:09 +0200

    add TitleColumn componenet with styles

--- src/assets/components/Callendar/TitleColumn/TitleColumn.js
+++ src/assets/components/Callendar/TitleColumn/TitleColumn.js
@@ -4,8 +4,23 @@ import styles from "./TitleColumn.module.scss";
 const TitleColumn = () => (
   <>
     <div className={styles.column__container}>
-      <div className={styles.breakfast}>
+      <div className={styles.grid__bigUpper}>
         <p>6:00 AM</p>
+      </div>
+      <div className={styles.grid__medium}>
+        <p>9:00 AM</p>
+      </div>
+      <div className={styles.grid__medium}>
+        <p>12:00 PM</p>
+      </div>
+      <div className={styles.grid__medium}>
+        <p>3:00 PM</p>
+      </div>
+      <div className={styles.grid__bigLower}>
+        <p>6:00 PM</p>
+      </div>
+      <div className={styles.grid__small}>
+        <p>Workout</p>
       </div>
     </div>
   </>
