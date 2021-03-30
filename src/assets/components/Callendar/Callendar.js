108dde0ae162b200849c0a4a74e3b1216892f74a
Mariusz Wojtkiewicz <wojtkiewicz89@gmail.com>
2021-03-30 20:04:07 +0200

    add some styles to daycolumn component

--- src/assets/components/Callendar/Callendar.js
+++ src/assets/components/Callendar/Callendar.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./Callendar.module.scss";
 import TitleColumn from "./TitleColumn/TitleColumn";
+import DayColumn from "./DayColumn/DayColumn";
 
 const Callendar = () => (
   <>
     <div className={styles.callendar__container}>
       <TitleColumn />
+      <DayColumn />
       {/* tu będą 3 rodzaje column/ tytułowa/ robocza/ i dnia 7 */}
     </div>
   </>
---
+++ src/assets/components/Callendar/DayColumn/DayColumn.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styles from "./DayColumn.module.scss";
+
+const DayColumn = () => (
+  <>
+    <div className={styles.column__container}>
+      <div className={styles.grid__day}>
+        <p>DAY 1</p>
+      </div>
+      <div className={styles.grid__meal}>
+        <p>śniadanie</p>
+        <p>OK</p>
+      </div>
+      <div className={styles.grid__meal}>
+        <p>2 śniadanie</p>
+        <p>OK</p>
+      </div>
+      <div className={styles.grid__meal}>
+        <p>obiad</p>
+        <p>OK</p>
+      </div>
+      <div className={styles.grid__meal}>
+        <p>podwieczorek</p>
+        <p>OK</p>
+      </div>
+      <div className={styles.grid__meal}>
+        <p>kolacja</p>
+        <p>OK</p>
+      </div>
+      <div className={styles.grid__carb}>
+        <p>LOW-CARB</p>
+      </div>
+      <div className={styles.grid__training}>
+        <p>hantle</p>
+      </div>
+    </div>
+  </>
+);
+
+export default DayColumn;
