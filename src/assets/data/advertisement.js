402d03ef2c92f07c1577c52dd9e9e96bf7ca9abf
Mariusz Wojtkiewicz <wojtkiewicz89@gmail.com>
2021-03-31 12:03:00 +0200

    add proper buttons to commercial, add properties to advertisement data

--- src/assets/data/advertisement.js
+++ src/assets/data/advertisement.js
@@ -3,15 +3,19 @@ const add = [
     title: "Running out of products?",
     text:
       "Lorem ipsum dolar sit amet, consect turadipscing elit. Integer aliquet.",
+    button: true,
   },
   {
     title: "Bod-e Trainer in your pocket",
     text:
       "Lorem ipsum dolar sit amet, consect turadipscing elit. Integer aliquet.",
+    apple: true,
+    android: true,
   },
   {
     title: "Frequently Asked Questions",
     text:
       "Lorem ipsum dolar sit amet, consect turadipscing elit. Integer aliquet.",
+    button: true,
   },
 ];
