f56d8b67e1f87abee60d306a3e8907201394ac9f
Mariusz Wojtkiewicz <wojtkiewicz89@gmail.com>
2021-03-31 11:41:34 +0200

    add advertisement data

--- src/assets/components/Add/ButtonAdd/ButtonAdd.js
+++ src/assets/components/Add/ButtonAdd/ButtonAdd.js
@@ -3,9 +3,10 @@ import styles from "./ButtonAdd.module.scss";
 
 const ButtonAdd = ({ text }) => (
   <>
-    <a href="/">
-      <p>{text}</p>
-      <div></div>
+    <a href="/" className={styles.wrapper}>
+      <div className={styles.image__container}></div>
+      <p className={styles.text}>{text}</p>
+      <div className={styles.arrow__container}></div>
     </a>
   </>
 );
---
+++ src/assets/data/advertisement.js
@@ -0,0 +1,17 @@
+const add = [
+  {
+    title: "Running out of products?",
+    text:
+      "Lorem ipsum dolar sit amet, consect turadipscing elit. Integer aliquet.",
+  },
+  {
+    title: "Bod-e Trainer in your pocket",
+    text:
+      "Lorem ipsum dolar sit amet, consect turadipscing elit. Integer aliquet.",
+  },
+  {
+    title: "Frequently Asked Questions",
+    text:
+      "Lorem ipsum dolar sit amet, consect turadipscing elit. Integer aliquet.",
+  },
+];
